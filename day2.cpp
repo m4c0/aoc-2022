f759be7d8e5fcc41566ed3bbecf8b9e5b1a4459c
Eduardo Costa <m4c0@users.noreply.github.com>
2023-11-13 15:28:52 +0000

    day 2 part 1

---
+++ day2.cpp
@@ -0,0 +1,38 @@
+#pragma leco tool
+import jute;
+import silog;
+import sires;
+
+int main() {
+  auto file = sires::slurp("../../day2.txt").take([](auto e) {
+    silog::log(silog::error, "failure: %s", e);
+    throw 1;
+  });
+  silog::log(silog::info, "file size: %d", file.size());
+
+  int points = 0;
+
+  jute::view f{file.begin(), file.size()};
+  while (f != "") {
+    auto [line, rest] = f.split('\n');
+    auto foe = line[0] - 'A';
+    auto me = line[2] - 'X';
+
+    auto pta = me + 1;
+
+    auto ptb = 0;
+    if (me == (foe + 1) % 3) {
+      ptb = 6;
+    } else if (me == foe) {
+      ptb = 3;
+    }
+
+    points += pta + ptb;
+
+    silog::log(silog::debug, "%d %d - %d %d - %d", foe, me, pta, ptb, points);
+
+    f = rest;
+  }
+
+  silog::log(silog::info, "points: %d", points);
+}
