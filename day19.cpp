3b6ba5dfb99d2e3a1af7617325a9e63eb405a5b0
Eduardo Costa <m4c0@users.noreply.github.com>
2023-11-25 10:14:36 +0000

    day 19 - part 1 - first draft

--- day19.cpp
+++ day19.cpp
@@ -18,6 +18,86 @@ auto next_int(jute::view v) {
   }
   throw 0;
 }
+
+struct bprint {
+  unsigned id;
+  unsigned oro;
+  unsigned cro;
+  unsigned obro;
+  unsigned obrc;
+  unsigned gro;
+  unsigned grob;
+};
+struct simul {
+  unsigned depth{};
+  unsigned o_bots{1};
+  unsigned c_bots{};
+  unsigned b_bots{};
+  unsigned g_bots{};
+  unsigned os{};
+  unsigned cs{};
+  unsigned bs{};
+  unsigned gs{};
+};
+simul bump(simul s) {
+  s.depth++;
+  s.os += s.o_bots;
+  s.cs += s.c_bots;
+  s.bs += s.b_bots;
+  s.gs += s.g_bots;
+  return s;
+}
+
+int step(const bprint &bp, const simul &s, int mins) {
+  if (mins == 0) {
+    return s.gs;
+  }
+  // silog::log(silog::debug, "%d %d %d %d - %d", s.o_bots, s.c_bots, s.b_bots,
+  // s.g_bots, s.depth);
+  mins--;
+
+  // wait?
+  int max{};
+
+  // build geode?
+  if (s.os >= bp.gro && s.cs >= bp.grob) {
+    simul ss = bump(s);
+    ss.g_bots++;
+    ss.os -= bp.gro;
+    ss.bs -= bp.grob;
+    mx(max, step(bp, ss, mins));
+  }
+
+  // build obsidian?
+  if (s.os >= bp.obro && s.cs >= bp.obrc) {
+    simul ss = bump(s);
+    ss.b_bots++;
+    ss.os -= bp.obro;
+    ss.cs -= bp.obrc;
+    mx(max, step(bp, ss, mins));
+  }
+
+  // build clay?
+  if (s.os >= bp.cro) {
+    simul ss = bump(s);
+    ss.c_bots++;
+    ss.os -= bp.cro;
+    mx(max, step(bp, ss, mins));
+  }
+
+  // build ore?
+  if (s.os >= bp.oro) {
+    simul ss = bump(s);
+    ss.o_bots++;
+    ss.os -= bp.oro;
+    mx(max, step(bp, ss, mins));
+  }
+
+  mx(max, step(bp, bump(s), mins));
+
+  return max;
+}
+
 int main() {
   loop_e([&](auto line) {
     auto [bps, bpr] = next_int(line);
@@ -28,14 +108,21 @@ int main() {
     auto [gos, gor] = next_int(bcr);
     auto [gbs, gbr] = next_int(gor);
 
-    auto bp = atoi(bps);
-    auto o = atoi(os);
-    auto c = atoi(cs);
-    auto bo = atoi(bos);
-    auto bc = atoi(bcs);
-    auto go = atoi(gos);
-    auto gb = atoi(gbs);
-
-    silog::log(silog::info, "%d %d %d %d %d %d %d", bp, o, c, bo, bc, go, gb);
+    bprint b{
+        atoi(bps), atoi(os),  atoi(cs),  atoi(bos),
+        atoi(bcs), atoi(gos), atoi(gbs),
+    };
+    info("g", step(b, {}, 24));
   });
+
+  // o (w) o1
+  // o o1 (w) o2
+  // o o2 (c,w) +c o1
+  // o+c o1 (w) o2 c1
+  // o+c o2 c1 (c,w) +c o1 c2
+  // o+2c o1 c2 (w) o2 c4
+  // o+2c o2 c4 (c,w) +c o1 c6
+  // o+3c o1 c6 (w) o2 c9
+  // o+3c o2 c9 (c,w) o3 c12
+  // o+3c o3 c12 (c,w) o4 c15
 }
